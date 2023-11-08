82a5cf6e5d461c3035cb81c07e46d871ce9d04c5
phagocyte2022 <mp.forcode@gmail.com>
2023-11-08 12:08:47 +0200

    lesson-126. Spring Framework. Data JPA. Query creation

--- ext/ext-systems/register-office/src/main/java/edu/javacourse/ext/register/dao/MarriageDao.java
+++ ext/ext-systems/register-office/src/main/java/edu/javacourse/ext/register/dao/MarriageDao.java
@@ -2,9 +2,18 @@ package edu.javacourse.ext.register.dao;
 
 import edu.javacourse.ext.register.domain.MarriageCertificate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MarriageDao extends JpaRepository <MarriageCertificate, Long>{
 
+    List<MarriageCertificate> findByNumber (String number);
+    List<MarriageCertificate> findByNum (@Param("number") String number);
+
+    @Query("SELECT mc FROM MarriageCertificate mc WHERE mc.number = :number")
+    List<MarriageCertificate> findSomething (@Param("number") String number);
 }
