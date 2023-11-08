953612d2691219bbb8542f1c01a97927fe99584b
phagocyte2022 <mp.forcode@gmail.com>
2023-11-08 21:19:38 +0200

    lesson-131. Student Check. webapp finaliziation

--- ext/ext-systems/student-check/src/main/java/edu/javacourse/ext/student/service/StudentService.java
+++ ext/ext-systems/student-check/src/main/java/edu/javacourse/ext/student/service/StudentService.java
@@ -1,6 +1,5 @@
 package edu.javacourse.ext.student.service;
 
-import com.sun.xml.bind.v2.util.CollisionCheckStack;
 import edu.javacourse.ext.student.dao.StudentRepository;
 import edu.javacourse.ext.student.domain.Student;
 import edu.javacourse.ext.student.domain.StudentDocument;
@@ -10,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +22,7 @@ public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
+    @Transactional
     public List<StudentResponse> getStudentInfo(StudentRequest request){
         List<Student> student = studentRepository.findStudent(request.getLasttName(),
                 request.getFirstName(),
--- ext/ext-systems/student-check/src/test/java/edu/javacourse/ext/student/service/StudentServiceTest.java
+++ ext/ext-systems/student-check/src/test/java/edu/javacourse/ext/student/service/StudentServiceTest.java
@@ -1,5 +1,8 @@
 package edu.javacourse.ext.student.service;
 
+import edu.javacourse.ext.student.view.StudentRequest;
+import edu.javacourse.ext.student.view.StudentResponse;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = {"classpath:springContext.xml"})
 public class StudentServiceTest {
@@ -15,12 +21,22 @@ public class StudentServiceTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceTest.class);
 
     @Autowired
-    private StudentService studentService;
+    private StudentService studentController;
 
     @Test
-    public void simpleTest() {
-        studentService.simpleCall();
-        LOGGER.info("DONE !!!");
+    public void studentInfo() {
+        StudentRequest req = new StudentRequest();
+        req.setLasttName("last");
+        req.setFirstName("first");
+        req.setMiddletName("middle");
+        req.setDateOfBirth(LocalDate.of(2000, 04, 12));
+        req.setPassportSeria("1111");
+        req.setPassportNumber("222222");
+        req.setPassportDate(LocalDate.of(2016, 04, 30));
+
+        List<StudentResponse> studentInfo = studentController.getStudentInfo(req);
+        Assert.assertTrue(studentInfo.size()>0);
+
     }
 
 }
